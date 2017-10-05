73d10f941d720081d5b3bfe4afa1fdf037e78eb7
Gavin <15sic045x@sit.singaporetech.edu.sg>
2017-10-05 16:58:34 +0800

    Updated view user

--- GymLife/admin/user.php
+++ GymLife/admin/user.php
@@ -1,115 +1,163 @@
-<?php include '../conn.php'; ?>
-<?php include '../session/adminSession.php'; ?>
-<!DOCTYPE html>
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Factornator</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+﻿<?php include '../conn.php'; ?>
+<?php require_once('../session/adminSession.php'); ?>
 
-        <!-- Google Webfonts -->
-        <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100,500' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<!doctype html>
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><html lang="en" class="no-js"> <![endif]-->
+<html lang="en">
 
-        <!-- Animate.css -->
-        <link rel="stylesheet" href="../assets/css/animate.css">
-        <!-- Icomoon Icon Fonts-->
-        <link rel="stylesheet" href="../assets/css/icomoon.css">
-        <!-- Owl Carousel -->
-        <link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
-        <link rel="stylesheet" href="../assets/css/owl.theme.default.min.css">
-        <!-- Magnific Popup -->
-        <link rel="stylesheet" href="../assets/css/magnific-popup.css">
-        <!-- Theme Style -->
-        <link rel="stylesheet" href="../assets/css/style.css">
-        <!--SweetAlert-->
-        <link rel="stylesheet" href="../assets/plugins/sweetalert-master/sweet-alert.css">
-        <!-- jQuery -->
-        <script src="../assets/js/jquery.min.js"></script>
-        <!--Datatable-->
-        <link rel="stylesheet" href="../assets/plugins/DataTables/css/jquery.dataTables.css">
-        <script src="../assets/plugins/DataTables/js/jquery.dataTables.js"></script>
-        <script src="https://cdn.datatables.net/responsive/2.1.1/js/dataTables.responsive.min.js"></script>
-        <script src="https://cdn.datatables.net/responsive/2.1.1/js/responsive.bootstrap.min.js"></script>
-        <!-- Modernizr JS -->
-        <script src="../assets/js/modernizr-2.6.2.min.js"></script>
-        <script src="../assets/js/custom.js"></script>
-        <script>
-            $(document).ready(function () {
-                $('#users').DataTable();
-            });
-        </script>
-    </head>
-    <body>
-        <?php include '../header.php'; ?>
-        <!-- END .header -->
+<head>
 
-        <aside class="fh5co-page-heading">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h1 class="fh5co-page-heading-lead">
-                            Account Management
-                            <span class="fh5co-border"></span>
-                        </h1>
+    <!-- Basic -->
+    <title>GymLife | Home</title>
 
-                    </div>
-                </div>
-            </div>
-        </aside>
+    <!-- Define Charset -->
+    <meta charset="utf-8">
 
-        <div id="fh5co-main">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <table id="users" class="display" cellspacing="0" width="100%">
-                            <thead>
-                                <tr>
-                                    <th>Username</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Role</th>
-                                    <th data-sortable="false">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $record = DB::query("SELECT * FROM user");
+    <!-- Responsive Metatag -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-                                foreach ($record as $row) {
-                                    echo "<tr>";
-                                    echo "<td>" . $row['username'] . "</td>";
-                                    echo "<td>" . $row['name'] . "</td>";
-                                    echo "<td>" . $row['email'] . "</td>";
-                                    echo "<td>" . $row['role'] . "</td>";
-                                    echo "<td>";
-                                    echo "<button class='btn btn-primary' onclick=\"location.href='viewUser.php?id=" . $row['userid'] . "'\">View</button>";
-                                    echo "<button class='btn btn-success' onclick=\"location.href='edituser.php?id=" . $row['userid'] . "'\">Edit</button>";
-                                    echo "</td>";
-                                    echo "</tr>";
+    <!-- Page Description and Author -->
+    <meta name="description" content="Sulfur - Responsive HTML5 Template">
+    <meta name="author" content="Shahriyar Ahmed">
+
+    <!-- Bootstrap CSS  -->
+    <link rel="stylesheet" href="../asset/bootstrap/css/bootstrap.min.css" type="text/css">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="../asset/font-awesome/css/font-awesome.min.css" type="text/css">
+
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="../asset/css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="../asset/css/owl.theme.css" type="text/css">
+    <link rel="stylesheet" href="../asset/css/owl.transitions.css" type="text/css">
+
+    <!-- Css3 Transitions Styles  -->
+    <link rel="stylesheet" type="text/css" href="../asset/css/animate.css">
+
+    <!-- Lightbox CSS -->
+    <link rel="stylesheet" type="text/css" href="../asset/css/lightbox.css">
+
+    <!-- Sulfur CSS Styles  -->
+    <link rel="stylesheet" type="text/css" href="../asset/css/style.css">
+
+    <!-- Responsive CSS Style -->
+    <link rel="stylesheet" type="text/css" href="../asset/css/responsive.css">
+
+    <!-- jQuery -->
+    <script src="../asset/js/jquery-2.1.3.min.js"></script>
+
+    <!--Datatable-->
+    <link rel="stylesheet" href="../asset/plugins/DataTables/css/jquery.dataTables.css">
+    <script src="../asset/plugins/DataTables/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.1.1/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.1.1/js/responsive.bootstrap.min.js"></script>
+
+    <!-- Modernizr JS -->
+    <script src="../asset/js/modernizrr.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#users').DataTable();
+        });
+    </script>
+
+
+
+</head>
+
+<body>
+
+    <!--Navigation Section-->
+		<?php require_once('../header.php'); ?>
+
+
+    <div id="fh5co-main">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <table id="users" class="display" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Username</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                                <th data-sortable="false">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $record = DB::query("SELECT * FROM user WHERE status='2'");
+
+                            foreach ($record as $row) {
+                            echo "
+                            <tr>
+                                ";
+                                echo "
+                                <td>" . $row['name'] . "</td>";
+                                echo "
+                                <td>" . $row['userName'] . "</td>";
+                                echo "
+                                <td>" . $row['email'] . "</td>";
+                                if ($row['roleID'] == '1') {
+                                echo "
+                                <td> Admin </td>";
                                 }
-                                ?>
-                            </tbody>
-                        </table><!-- /userTable -->
-                    </div>
+                                elseif ($row['roleID']=='2'){
+                                echo "
+                                <td> Trainer </td>";
+                                }
+                                elseif ($row['roleID']=='3'){
+                                echo "
+                                <td> Trainee </td>";
+                                }
+                                else{
+                                echo "
+                                <td> ERROR </td>";
+                                }
+                                echo "
+                                <td>
+                                    ";
+                                    echo "<button class='btn btn-default' onclick=\"location.href ='viewUser.php?id=" . $row['userID'] . "' \">View</button>";
+                                    echo "<button class='btn btn-default' onclick=\"location.href ='edituser.php?id=" . $row['userID'] . "' \">Delete</button>";
+                                    echo "
+                                </td>";
+                                echo "
+                            </tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table><!-- /userTable -->
                 </div>
             </div>
         </div>
+    </div>
 
-        <!-- jQuery Easing -->
-        <script src="../assets/js/jquery.easing.1.3.js"></script>
-        <!-- Bootstrap -->
-        <script src="../assets/js/bootstrap.min.js"></script>
-        <!-- Owl carousel -->
-        <script src="../assets/js/owl.carousel.min.js"></script>
-        <!-- Waypoints -->
-        <script src="../assets/js/jquery.waypoints.min.js"></script>
-        <!-- Magnific Popup -->
-        <script src="../assets/js/jquery.magnific-popup.min.js"></script>
-        <!-- Main JS -->
-        <script src="../assets/js/main.js"></script>
-        <!--SweetAlert-->
-        <script src="../assets/plugins/sweetalert-master/sweet-alert.js"></script>
-    </body>
+    <!-- Start Copyright Section -->
+    <div id="copyright-section" class="copyright-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="copyright">
+                        Copyright © 2017. ICT3104 Software Management - Gym Booking System</a>
+                    </div>
+                </div>
+            </div><!--/.row -->
+        </div><!-- /.container -->
+    </div>
+    <!-- End Copyright Section -->
+    <!-- Sulfur JS File -->
+    <script src="../asset/js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="../asset/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../asset/js/owl.carousel.min.js"></script>
+    <script src="../asset/js/jquery.appear.js"></script>
+    <script src="../asset/js/jquery.fitvids.js"></script>
+    <script src="../asset/js/jquery.nicescroll.min.js"></script>
+    <script src="../asset/js/lightbox.min.js"></script>
+    <script src="../asset/js/count-to.js"></script>
+    <script src="../asset/js/styleswitcher.js"></script>
+
+
+</body>
 </html>
+

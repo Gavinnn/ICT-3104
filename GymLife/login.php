32f331ca9619fbfd2f59a315c5e8bd23f6b443d3
Gavin <15sic045x@sit.singaporetech.edu.sg>
2017-10-05 20:46:10 +0800

    Update Register

--- GymLife/login.php
+++ GymLife/login.php
@@ -6,7 +6,56 @@
         <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
         <link rel="stylesheet" type="text/css" href="asset/css/login.css">
+
+        <!--SweetAlert-->
+        <link rel="stylesheet" href="asset/plugins/sweetalert-master/sweet-alert.css">
+        <script src="asset/plugins/sweetalert-master/sweet-alert.js"></script>
+        <!--Custom Javascript-->
+        <script src="asset/js/custom.js"></script>
         <script>
+            function register() {
+                var confirm = false;
+                var username = $("#username").val();
+                var name = $("#name").val();
+                var email = $("#email").val();
+                var contact = $("#contact").val();
+                var role = $("#role").val();
+                var password = $("#pass").val();
+
+                if (username == "")
+                    displayErrorMsg("Please fill in the \"username\"field.");
+                else if (name == "")
+                    displayErrorMsg("Please fill in the \"Name\" field.");
+                else if (email == "")
+                    displayErrorMsg("Please fill in the \"Email\" field.");
+                else if (contact == "")
+                    displayErrorMsg("Please fill in the \"Contact Number\" field.");
+                else if (role == "" || role == null)
+                    displayErrorMsg("Please fill in the \"Role\" field.");
+                else if (password == "" || password.length < 8)
+                    displayErrorMsg("\"Password\" field is too short.");
+                else if (password.length >= 8) {
+                    $.ajax({
+                        url: "registerProcess.php",
+                        data: {'username': username, 'name': name, 'email': email, 'contact': contact, 'role': role, 'password': password},
+                        type: 'POST',
+                        async: false,
+                        success: function (data) {
+                            if (data == "success") {
+                                confirm = true;
+                                successModal("Successfully registered account", "index.php");
+
+                            } else if (data == "username") {
+                                displayErrorMsg("The username has been taken.");
+                            } else if (data == "email") {
+                                displayErrorMsg("The email has been used.");
+                            }
+                        }
+                    });
+                }
+
+                return confirm;
+            }
             function check()
             {
                 var check = false;
@@ -47,24 +96,27 @@
                             <label>
                                 Username<span class="req">*</span>
                             </label>
-                            <input type="text"required autocomplete="off"/>
+                            <input id="username" type="text"required autocomplete="off"/>
                         </div>
                         <div class="field-wrap">
                             <label>
                                 Name<span class="req">*</span>
                             </label>
-                            <input type="text"required autocomplete="off"/>
+                            <input id="name" type="text"required autocomplete="off"/>
                         </div>
                         <div class="field-wrap">
                             <label>
                                 Email Address<span class="req">*</span>
                             </label>
-                            <input type="email"required autocomplete="off"/>
+                            <input id="email" type="email"required autocomplete="off"/>
                         </div>
                         <div class="field-wrap">
                             <label>
-                                Role<span class="req">*</span>
+                                Contact Number<span class="req">*</span>
                             </label>
+                            <input id="contact" type="text"required autocomplete="off"/>
+                        </div>
+                        <div class="field-wrap">
                             <select id="role" name="role"> 
                                 <option value = '1' >trainee</option>
                                 <option value = '2'>trainer</option>
@@ -74,9 +126,9 @@
                             <label>
                                 Set A Password<span class="req">*</span>
                             </label>
-                            <input type="password"required autocomplete="off"/>
+                            <input id="pass" type="password"required autocomplete="off"/>
                         </div>
-                        <button type="submit" class="button button-block"/>Get Started</button>
+                        <button onClick="register();" type="button" class="button button-block">Get Started</button>
                     </form>
                 </div>
 
--- GymLife/registerProcess.php
+++ GymLife/registerProcess.php
@@ -1,44 +1,40 @@
 <?php
-include 'conn.php';
 
-$userid = mysqli_real_escape_string($conn, $_POST['userid']);
-$name =mysqli_real_escape_string($conn, $_POST['name']);
-$password = md5('password' . 'LRMS');
-$email =mysqli_real_escape_string($conn, $_POST['email']);
-$role = mysqli_real_escape_string($conn, $_POST['role']);
-(isset($_POST['faculty'])) ? $faculty = mysqli_real_escape_string($conn, $_POST['faculty']) : $faculty = $_SESSION['faculty'];    
-(isset($_POST['course'])) ? $course = mysqli_real_escape_string($conn,$_POST['course']) : $course = NULL;
-(isset($_POST['expertiseLevel'])) ? $expertiseLevel = mysqli_real_escape_string($conn,$_POST['expertiseLevel']) : $expertiseLevel = NULL;
-(isset($_POST['hoursClocked'])) ? $hoursClocked = mysqli_real_escape_string($conn,$_POST['hoursClocked']) : $hoursClocked = NULL;
-(isset($_POST['labSafetyTest'])) ? $labSafetyTest = mysqli_real_escape_string($conn,$_POST['labSafetyTest']) : $labSafetyTest = NULL;
-$lab = $_POST['lab'];
+//Config for database
+require_once('conn.php');
 
-$queryCheckUnicityOfUserid = "SELECT * FROM `user` WHERE `userid` = '$userid'";
-$resultCheckUnicityOfUserid = $conn->query($queryCheckUnicityOfUserid);
+//Post from form
+$username = post("username");
+$name = post("name");
+$email = post("email");
+$contact = post("contact");
+$role = post("role");
+$pass = hashPassword(post("password"));
 
-if($resultCheckUnicityOfUserid->fetch_assoc()){
-    echo "notUnique";
-}
-else{
-	$conn->begin_transaction();	
-    $query = "INSERT INTO `user` (`userid`, `password`, `name`, `email`, `role`, `faculty`, `courses`, `labSafetyTest`, `expertiseLevel`, `hoursClocked`) VALUES ('$userid', '$password', '$name', '$email', '$role', '$faculty', '$course', '$labSafetyTest', '$expertiseLevel', '$hoursClocked');";
-    $result = $conn->query($query);
-    
-    if($lab!=NULL && sizeof($lab) > 0){
-    	for($i = 0; $i < sizeof($lab); $i++){
-    		$labId = $lab[$i];
-    		$queryUserLab = "INSERT INTO userLab (userid, labId) VALUES ('$userid', '$labId');";
-    		$resultUserLab = $conn->query($queryUserLab);
-    	}
+//Query to select username
+$record = DB::queryFirstRow("SELECT userID FROM user WHERE username=%s", $username);
+
+//Check if username exist
+if ($record)
+    echo "username";
+else {
+    //Query to select email
+    $record = DB::queryFirstRow("SELECT userID FROM user WHERE email=%s", $email);
+    if ($record) {
+        echo "email";
+    } else {
+        $record = DB::insert('user', array(
+                    'username' => $username,
+                    'name' => $name,
+                    'email' => $email,
+					'contactNumber' => $contact,
+                    'roleID' => $role,
+                    'password' => $pass,
+                    'status' => 1
+        ));
+        if ($record) {
+            echo "success";
+        }
     }
-
-   if(($result && ($lab != null && $resultUserLab)) || ($result && $lab == null)){
-   		$conn->commit(); 
-    	echo "success";
-    }
-    else{    
-    	$conn->rollback();     
- 	   echo "failed";
-    } 
 }
 ?>

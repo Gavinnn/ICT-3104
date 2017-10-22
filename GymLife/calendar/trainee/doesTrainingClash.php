6521af68ed48088383cf0d68085fd89f94ac0ed9
muhammadbna <muhammadbna@gmail.com>
2017-10-23 02:02:53 +0800

    Trainee cannot book new trainings that coincides with any existing ones

---
+++ GymLife/calendar/trainee/doesTrainingClash.php
@@ -0,0 +1,24 @@
+<?php
+
+    // desc: to check whether the training the Trainee has selected clashes with any of their existing trainings
+
+    //Config for database
+    require_once('../../conn.php');
+
+    //DB class prevent SQL injection
+    $traineeID = htmlspecialchars(post("traineeID"), ENT_QUOTES);
+    $startTime = post("startTime");
+
+    // retrieve any trainings that clashes with the training that the Trainee intends to book
+    $record = DB::query("SELECT * FROM trainersessions WHERE traineeID = %s AND startSession = %s", $traineeID, $startTime);
+
+    // if there are trainings, does clashes. Return true
+    if ($record) {
+        echo "true";
+    } 
+    // if there are no trainings, does not clash. Return false
+    else{
+        echo "false";
+    }
+    
+?>
--- GymLife/calendar/trainee/traineeCalendar.php
+++ GymLife/calendar/trainee/traineeCalendar.php
@@ -101,7 +101,11 @@
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                         <h4 class="modal-title" id="myModalLabel">Confirm Training</h4>
                       </div>
-                      <div class="modal-body">
+                      <div class="modal-body" id="modal-body">
+
+                        <div class="alert alert-warning" role="alert" style="display:none;" id="alert">
+                            You already have a training in this time slot!
+                        </div>
 
                           <div class="form-group">
                             <label for="trainingTitle" class="col-sm-2 control-label">Training Title</label>
@@ -140,9 +144,9 @@
                       </div>
                       <div class="modal-footer">
                         <button type="button" id="closeButton" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button id="confirmButton" type="submit" class="btn btn-success">Confirm</button>
+                        <button id="confirmButton" type="submit" class="btn btn-success" onClick="doesTrainingClash()">Confirm</button>
                       </div>
-                    </form>
+                    </form>                    
                     </div>
                   </div>
                 </div>
@@ -180,16 +184,62 @@
     <script src="../../asset/js/count-to.js"></script>
     <script src="../../asset/js/styleswitcher.js"></script>
 
-    <!-- Render  HTML button for modal depending on availability of training -->
+    <!-- Render HTML elements in modal depending on use case -->
     <script>
-        // display a confirm button if the training is available. Else, do not display it
+        
+        // when modal is opened
         $('#ModalConfirm').on('shown.bs.modal', function () {
+            doesTrainingClash();
+        });
+
+        // when modal closes
+        $('#ModalConfirm').on('hidden.bs.modal', function () {
+
+            // if traiing is already booked, show the confirm button again so it appears in other training modals
             if ($("#confirmedTraineeID").val() != ""){
-                $("#confirmButton").hide();
+                $("#confirmButton").show();
+            }
+
+            // if alert is shown, hide it so it won't appear in other training modals
+            if ($("#alert")){
+                $("#alert").hide();
             }
         });
 
-    </script>
+        //---------------------------------------------------------------------------------------
+        // desc: to check whether the training the Trainee has selected clashes with any of 
+        // their existing trainings. If clashes, show alert and remove confirm button
+        //---------------------------------------------------------------------------------------
+        function doesTrainingClash(){
 
+            var traineeID = $("#traineeID").val();
+            var startTime = $("#startTime").val();
+
+            // ajax call to determine if there any clashes
+            $.ajax({
+                url: "doesTrainingClash.php",
+                data: {'traineeID' : traineeID, 'startTime': startTime},
+                type: 'POST',
+                async: false,
+                success: function (results) {
+
+                    // there is a clash...
+                    if (results == "true"){
+
+                        // the  training that was booked by the Trainee
+                        if ($("#confirmedTraineeID").val() != ""){
+                            $("#alert").hide();
+                            $("#confirmButton").hide();
+                        }
+                        // the new training that the Trainee intends to book but clashes with a previos training
+                        else{
+                            $("#alert").show();
+                            $("#confirmButton").hide();
+                        }
+                    }
+                }
+            });
+        }
+    </script>
         </body>
 </html>

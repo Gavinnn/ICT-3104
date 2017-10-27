c54fa93fa577e30c9522c1dd1290592294c8f4bf
muhammadbna <muhammadbna@gmail.com>
2017-10-27 09:13:59 +0800

    Trainer can add rooms, gyms, and descriptions in trainings

--- GymLife/calendar/trainer/addTraining.php
+++ GymLife/calendar/trainer/addTraining.php
@@ -6,22 +6,24 @@
 require_once('../../conn.php');
 
 // ensure all variables are set
-if (isset($_POST['title']) && isset($_POST['startTime']) && isset($_POST['endTime']) && isset($_POST['date']) && isset($_POST['trainerID'])){
+if (isset($_POST['title']) && isset($_POST['category']) && isset($_POST['gym']) && isset($_POST['rooms']) && isset($_POST['date']) && isset($_POST['startTime']) && isset($_POST['endTime']) && isset($_POST['description']) && isset($_POST['trainerID'])){
 
 	// retrieve the data
-	$title = $_POST['title'];
-	$startTime = $_POST['startTime'];
-	$endTime = $_POST['endTime'];
 	$trainerID = $_POST['trainerID'];
+	$title = $_POST['title'];
+	$locationID = $_POST['gym'];
+	$roomID = $_POST['rooms'];
+	$description = $_POST['description'];
+	$trainingID = $_POST['category'];
 	
 	// transform the data into a datetime string
-	$startDate = $_POST['date'] . " " . $startTime . ":00";	// add :00 to match DateTime format as seconds are missing
-	$endDate = $_POST['date'] . " " . $endTime . ":00";	// add :00 to match DateTime format as seconds are missing
+	$startSession = $_POST['date'] . " " . $_POST['startTime'] . ":00";	// add :00 to match DateTime format as seconds are missing
+	$endSession = $_POST['date'] . " " . $_POST['endTime'] . ":00";	// add :00 to match DateTime format as seconds are missing
 
 
 	// INSERT query
-	$status = DB::query("INSERT INTO trainersessions(title, startSession, endSession, trainerID)
-	 values (%s, %s, %s, %d)", $title, $startDate, $endDate, $trainerID);
+	$status = DB::query("INSERT INTO trainersessions(trainerID, title, locationID, roomID, description, trainingID, startSession, endSession)
+	 values (%d, %s, %d, %d, %s, %d, %s, %s)", $trainerID, $title, $locationID, $roomID, $description, $trainingID, $startSession, $endSession);
 }
 //Redirect to trainerCalendar page
 header('Location: trainerCalendar.php');
